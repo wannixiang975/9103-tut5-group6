3fe07ef499fa6dbfd405441666d5f2054b00e513
wanni xiang <wannixiang975@gmail.com>
2026-05-29 15:02:02 +1000

    Added the Java part of the team and the code for the "time" section.
    Added the Java part of the team and the code for the "time" section.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(900, 600);
+  angleMode(RADIANS);
+
+  // Setup functions from each mechanic file
+  setupAudioMechanic();
+  setupTimeMechanic();
+  setupPerlinRandomMechanic();
+  setupUserInputMechanic();
 }
 
 function draw() {
-  background(220);
+  background(245, 238, 215);
+
+  // Draw each mechanic layer
+  drawAudioMechanic();
+  drawTimeMechanic();
+  drawPerlinRandomMechanic();
+  drawUserInputMechanic();
 }
